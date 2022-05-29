4a717391690045a57fc6a3d7d7c19e15d9a2036b
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-05-29 18:04:11 +0630

    Implementing Login,Loginout,Register,Forget,Reset

--- src/common/Nav.jsx
+++ src/common/Nav.jsx
@@ -1,39 +1,79 @@
 import React, { Component } from 'react'
+import {
+     BrowserRouter as Router,Switch,Route,Link
+   } from "react-router-dom";
 
-export class Nav extends Component {
-    render() {
-        return (
-            <div>
-                <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-                    <a class="navbar-brand" href="#">Navbar</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarText">
-                        <ul class="navbar-nav mr-auto">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Profile</a>
-                            </li>
-                        </ul>
-                        <span class="navbar-text">
-                            <ul class="navbar-nav mr-auto">
-                                
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Login</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Register</a>
-                                </li>
-                            </ul>
-                        </span>
+class Nav extends Component {
+
+     state = {
+          loggedout:''
+     }
+     logout = () => {
+          localStorage.clear();
+          this.props.setUser(null);
+     }
+
+     render() {
+
+          let buttons;
+          let profile;
+          if(localStorage.getItem('token')){
+               buttons = (
+                    <div>
+         <Link class="nav-link" to="/" onClick={this.logout} >Logout  </Link>   
                     </div>
-                </nav>
-            </div>
-        )
-    }
+               )
+               profile = (
+                    <div>
+         <Link class="nav-link" to="/profile">Profile</Link>   
+                    </div>
+               )
+          }else{
+               buttons = (
+                    <div>
+    <ul class="navbar-nav mr-auto">
+    <li class="nav-item">
+        <Link class="nav-link" to="/login">Login </Link>
+      </li>
+      
+      <li class="nav-item">
+        <Link class="nav-link" to="/register">Register</Link>
+      </li>
+    </ul>         
+                    </div>
+               )
+
+          }
+
+          return (
+               <div>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <Link class="navbar-brand" to="/">Easy Learning</Link>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <Link class="nav-link" to="/">Home <span class="sr-only">(current)</span></Link>
+      </li>
+      
+      <li class="nav-item">
+        {profile}
+      </li>
+    </ul>
+    <span class="navbar-text">
+
+        {buttons}
+
+    </span>
+  </div>
+</nav>
+                    
+               </div>
+          )
+     }
 }
 
 export default Nav
--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Header from './common/Header';
 import reportWebVitals from './reportWebVitals';
-import { Header } from './common/Header';
+import axios from 'axios';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// set main base url 
+axios.defaults.baseURL = 'http://127.0.0.1:8000/api';
+// Beare Token Save 
+axios.defaults.headers.common['Authorization']='Bearer '+localStorage.getItem('token')
+
+ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Header/>
-  </React.StrictMode>
+    <Header />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
